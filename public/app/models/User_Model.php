510ee6f9e9c9150581b44a6c0b584067c6638c4c
iqbalthex <73935864+iqbalthex@users.noreply.github.com>
2022-11-22 15:06:11 +0700

    Delete public/app directory

--- public/app/models/User_Model.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class User_Model{
-	private $nama = 'Iqbal Arie Maulana';
-
-	public function get_user(){
-		return $this->nama;
-	}
-}
--- public/app/views/home/index.php
+++
@@ -1,9 +0,0 @@
-<div class="container mt-4">
-	<div class="p-5 mb-4 bg-light rounded-3">
-		<div class="container-fluid py-5">
-			<h1 class="display-5 fw-bold">Selamat Datang di Website Saya</h1>
-			<p class="col-md-8 fs-4">Halo, nama saya <?= $data['name'] ?></p>
-			<a class="btn btn-primary btn-lg" type="button" href="about">Tentang Saya</a>
-		</div>
-	</div>
-</div>
